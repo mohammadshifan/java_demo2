7cba05fd3afd5e649589a1ff336dd66341d9e3c1
mohammadshifan <159225296+mohammadshifan@users.noreply.github.com>
2024-02-13 20:05:26 -0800

    Add files via upload

---
+++ classdistance.java
@@ -0,0 +1,16 @@
+class distance
+{
+  int meters;
+  int cms;
+public void setdata(int m,int cm)
+  {
+   meters=m;
+   cms=cm;
+  }
+public void display()
+  {
+   System.out.println("meters="+meters);
+   System.out.println("cms="+cms);
+  }
+}
+   
